e5b97be8236ff63157039e51c94cfaf3fadbfa4e
jed1-sketch <rojedkwao@gmail.com>
2020-04-30 09:52:03 +0000

    changes to the former clssmthd.py

--- clssmthd.py
+++ clssmthd.py
@@ -1,2 +1,27 @@
+
+class VotersID:
+
+    next_serial=2469
+
     @classmethod
-        return result
+
+    def _get_next_serial(cls):
+        result=cls.next_serial
+        cls.next_serial += 1
+        # return result  we will only add this when we wantit to run automatically
+
+    
+    @classmethod
+    def create_empty(cls,owner_code):
+
+        return cls (owner_code,contents=None)
+
+    @classmethod
+    def create_with_items(cls,owner_code,items):
+        return cls (owner_code,contents=list(items) )
+
+
+    def __init__(self,owner_code,contents):
+        self.owner_code=owner_code
+        self.contents=contents
+        self.serial=VotersID._get_next_serial()     
